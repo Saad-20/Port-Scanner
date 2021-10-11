605b1787f89597906e879d7c2392d83d23437167
Saad-20 <m.saadshahzad@gmail.com>
2021-10-11 13:41:52 +0500

    Updated Readme and added comprehensive scan function.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Port-Scanner
-This is a port scanner i am building after the famous nmap tool. I am building this tool for the purpose of automating my usage for nmap
+This is a port scanner script written in python3. I am building after the famous nmap tool. I am building this tool for the purpose of automating my usage for nmap. The libraries that are being used in this script are pyfiglet and nmap
+ 
--- scanner.py
+++ scanner.py
@@ -4,11 +4,13 @@ import nmap
 
 Scanner = nmap.PortScanner()
 
-ip_Address = input("Enter Ip address: ")
-
 def banner():
- ascii_banner = pyfiglet.figlet_format("LETS SNIFF :D")
+ ascii_banner = pyfiglet.figlet_format("SNIFF THOSE PORTS OUT :D")
  print(ascii_banner)
+ print("@Author: Saad Shahzad\n")
+banner()
+
+ip_Address = input("Enter Ip address: ")
 
 selection = input("""Select the type of scan:-
 	    1)Regular Scan
@@ -17,11 +19,23 @@ selection = input("""Select the type of scan:-
 	    4)UDP Scan
 	    5)Ping Scan
 	    6)OS Detection Scan
-	    7)Multiple IP""")
+	    7)Multiple IP\n""")
 
 print("you have selected",selection)
 
 if selection == '1':
  Scanner.scan(ip_Address)
- print(Scanner.scaninfo())
- print("Ip Status", Scanner[ip_Address].state())
+ print("Ip Status: {0}".format(Scanner[ip_Address].state()))
+ for host in Scanner.all_hosts():
+  for proto in Scanner[host].all_protocols():
+   lport = Scanner[host][proto].keys()
+   for port in sorted(lport):
+    print("port: {0} state: {1}".format(port, Scanner[host][proto][port]['state']))
+
+elif selection == '2':
+ Scanner.scan(ip_Address, '1-1024', '-v -sS -sV -sC -A -O')
+ print("Ip Status: ", Scanner[ip_Address].state())
+ print("Open Ports: ", Scanner[ip_Address]['tcp'].keys())
+ print(Scanner[ip_Address].all_protocols())
+else:
+ print("Please enter a selection")
